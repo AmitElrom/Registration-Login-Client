af99f258988598854fcf80050e7a086ee0b9307b
Amit Elrom <amitelrom99@gmail.com>
2022-05-10 09:03:56 +0300

    test async

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,30 +1,38 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 // form managing hooks
 import { useForm } from 'react-hook-form'
 import * as yup from 'yup'
 import { yupResolver } from '@hookform/resolvers/yup'
 
-const shema = yup.object().shape({
+const schema = yup.object().shape({
     email : yup.string().email().required(),
     password : yup.string().required() 
 })
  
-
 const Login = () => {
-  return (
-    <div style={{ textAlign : 'center', border : '.3em solid black', padding : '1em', width : 'fit-content', margin : 'auto' }}>
-        <h3>Login</h3>
-        <form>
-        <input type="text" placeholder='email' /><br />
-        <input type="text" placeholder='password' /><br />
-        <input type="submit" value='Sign In' /><br /><br />
-        Not A Member? <Link to="/register">Sign Up Now</Link>
-        
-        </form>
-    </div>
-  )
+
+    const { register, handleSubmit, setValue, formState : { errors } } = useForm({
+        resolver : yupResolver(schema)
+    })
+
+    const submitForm = (data) => {
+        console.log(data);
+    }
+
+    return (
+        <div style={{ textAlign : 'center', border : '.3em solid black', padding : '1em', width : 'fit-content', margin : 'auto' }}>
+            <h3>Login</h3>
+            <form onSubmit={handleSubmit(submitForm)} >
+                <input type="text" placeholder='email' {...register('email')} /><br />
+                <input type="text" placeholder='password' {...register('password')} /><br />
+                <input type="submit" value='Sign In' /><br /><br />
+                Not A Member? <Link to="/register">Sign Up Now</Link>
+            </form>
+        </div>
+    )
 }
 
 export default Login
--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,19 +1,39 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import axios from 'axios'
+
+// form managing hooks
+import { useForm } from 'react-hook-form'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+
+const schema = yup.object().shape({
+    email : yup.string().email().required(),
+    password : yup.string().required() 
+})
 
 const Registration = () => {
-  return (
-    <div style={{ textAlign : 'center', border : '.3em solid black', padding : '1em', width : 'fit-content', margin : 'auto' }}>
-        <h3>Sign Up</h3>
-        <form>
-        <input type="text" placeholder='email' /><br />
-        <input type="text" placeholder='password' /><br />
-        <input type="submit" value='Sign Up' /><br /><br />
-        Already A Member? <Link to="/">Log In</Link>
-        
-        </form>
-    </div>
-  )
+
+    const { register, handleSubmit, setValue, formState : { errors } } = useForm({
+        resolver : yupResolver(schema)
+    })
+
+    const submitForm = async (obj) => {
+        const { data } = await axios.post("http://localhost:8000/signup", obj)
+        console.log(data);
+    }
+
+    return (
+        <div style={{ textAlign : 'center', border : '.3em solid black', padding : '1em', width : 'fit-content', margin : 'auto' }}>
+            <h3>Sign Up</h3>
+            <form onSubmit={handleSubmit(submitForm)} >
+                <input type="text" placeholder='email' {...register('email')} /><br />
+                <input type="text" placeholder='password' {...register('password')} /><br />
+                <input type="submit" value='Sign Up' /><br /><br />
+                Already A Member? <Link to="/">Log In</Link>
+            </form>
+        </div>
+    )
 }
 
 export default Registration
